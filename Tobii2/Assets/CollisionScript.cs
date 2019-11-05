39bdefe2de7612b3adbc6b98d5253837b965725d
Peter Blanchfield <pxb@cs.nott.ac.uk>
2019-11-05 11:26:42 +0000

    Simple project eye tracked music player
    Using ideas from Si

---
+++ Tobii2/Assets/CollisionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+    AudioSource source;
+    public AudioClip clip;
+    float timeon = 0;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+	}
+	
+    public void OnCollisionStay2D(Collision2D col){
+        timeon += Time.deltaTime;
+        if (timeon > 0.5f)
+        {
+            timeon = 0;
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        timeon = 0;
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+    }
+}
---
+++ Tobii2/Assets/GetGaze.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tobii.Gaming;
+
+public class GetGaze : MonoBehaviour {
+    Tobii.Gaming.GazePoint gp;
+    Transform t;
+    float left = -9;
+    float right = 9;
+    float up = 4;
+    float down = -4;
+    public Vector2 gazePoint;
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+         
+        t = GetComponent<Transform>();
+        TobiiAPI.SetCurrentUserViewPointCamera(cam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gp = TobiiAPI.GetGazePoint();
+        Vector3 p = t.position;
+        float z = p.z;
+        gazePoint = gp.Viewport;
+        float x = gazePoint.x * 18f + left;
+        float y = gazePoint.y * 8f + down;
+        t.position = new Vector3(x, y, z);
+	}
+}
